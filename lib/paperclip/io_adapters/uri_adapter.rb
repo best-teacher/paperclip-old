4e98fbe707bdf988b1535523e7e8e1f419e51110
hitsujiwool <utatanenohibi@gmail.com>
2012-07-18 10:46:20 +0900

    画像のURLを渡したらfetchして保存できるようにした。

---
+++ lib/paperclip/io_adapters/uri_adapter.rb
@@ -0,0 +1,40 @@
+module Paperclip
+  class UriAdapter < AbstractAdapter
+    def initialize(target)
+      @target = open(target)
+      cache_current_values
+      
+      if @target.respond_to?(:tempfile)
+        @tempfile = copy_to_tempfile(@target.tempfile)
+      else
+        @tempfile = copy_to_tempfile(@target)
+      end
+    end
+
+    private
+
+    def cache_current_values
+      @original_filename = File.basename(@target.base_uri.path)
+      @content_type = @target.content_type
+      @size = File.size(@target.path)
+    end
+
+    def copy_to_tempfile(src)
+      while data = src.read(16*1024)
+        destination.write(data)
+      end
+      destination.rewind
+      destination
+    end
+  end
+end
+
+Paperclip.io_adapters.register Paperclip::UriAdapter do |target|
+  begin
+    uri = URI.parse(target)
+  rescue URI::InvalidURIError
+    false
+  else
+    true
+  end
+end
